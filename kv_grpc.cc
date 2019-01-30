688edb247e0628d2a48432c815fc8df69805d975
Stephanie <slampota@usc.edu>
2019-01-30 07:42:08 +0000

    grpc attemps

---
+++ kv_grpc.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#ifdef BAZEL_BUILD
+
+#else
+
+#endif
+
+// Logic and data behind the server's behavior.
+class ChirpServiceImpl final : public chirp::KeyValueStore::Service {
+  public:
+    Status Put(ServerContext* context, const chirp::PutRequest* request, chirp::PutReply* reply) override {
+      // TODO: find out how to generate random keys
+      int key = 0;
+      kvbe.Put(key, request);
+      return Status::OK;
+    }
+
+    Status Get(ServerContext* context, const chirp::GetRequest* request, chirp::GetReply* reply) override {
+      // TODO: call Get in the KeyValueDataStore and then place output into reply
+      *reply = kvbe.Get(request);
+      return Status::OK;
+    }
+
+    Status DeleteKey(ServerContext* context, const chirp::DelteKeyRequest* request, chirp::DeleteKeyReply* reply) override {
+      // TODO: call DeleteKey in the KeyValueDataStore
+      kvbe.DeleteKey(request);
+      return Status::OK;
+    }
+  private:
+    KeyValueBackEnd kvbe;
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50000");
+  ChirpServiceImpl service;
+
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
+  return 0;
+}
