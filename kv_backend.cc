232c4a9eb9fb6dc53d98b7b36e6268f2620e43a7
Stephanie <slampota@usc.edu>
2019-02-01 00:40:54 +0000

    addressed the comments that i understoof

--- kv_backend.cc
+++ kv_backend.cc
@@ -1,18 +1,18 @@
 #include <string>
 #include <map>
 
+// KeyValueBackEnd class to store all serialized data as bytes
 class KeyValueBackEnd {
 	public:
 		KeyValueBackEnd();
 		~KeyValueBackEnd();
-		std::string Get(std::string key);
-		int Put(std::string key, std::string chirp_value);
-		int DeleteKey(std::string key);
-		int size();
+		std::string Get(const std::string& key) const;
+		int Put(const std::string& key, const std::string& chirp_value);
+		int DeleteKey(const std::string& key);
+		int size() const;
 	private:
-		// key - std::string, value - serialized chirp object
+		// map to store data of key - std::string, value - serialized chirp object
 		std::map<std::string, std::string> chirp_map_;
-		
 };
 
 KeyValueBackEnd::KeyValueBackEnd() {
@@ -24,23 +24,27 @@ KeyValueBackEnd::~KeyValueBackEnd() {
 	
 }
 
-std::string KeyValueBackEnd::Get(std::string key) {
+// retrieve values with a string key
+std::string KeyValueBackEnd::Get(const std::string& key) const {
 	return chirp_map_.at(key);
 }
 
-int KeyValueBackEnd::Put(std::string key, std::string chirp_value) {
-	std::pair<std::string, std::string> createChirp;
-	createChirp.first = key;
-	createChirp.second = chirp_value;
-	chirp_map_.insert(createChirp);
+// insert a key and value into the chirp_map_
+int KeyValueBackEnd::Put(const std::string& key, const std::string& chirp_value) {
+	std::pair<std::string, std::string> createPair;
+	createPair.first = key;
+	createPair.second = chirp_value;
+	chirp_map_.insert(createPair);
 	return 0;
 }
 
-int KeyValueBackEnd::DeleteKey(std::string key) {
+// delete key from the chirp_map_
+int KeyValueBackEnd::DeleteKey(const std::string& key) {
 	chirp_map_.erase(key);
 	return 0;
 }
 
-int KeyValueBackEnd::size() {
+// function to keep track of the size of the backend
+int KeyValueBackEnd::size() const {
 	return chirp_map_.size();
 }
