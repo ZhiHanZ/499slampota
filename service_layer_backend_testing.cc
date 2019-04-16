918171072c7a9ecd0d8484c009a2a1aa990c4dc8
vagrant <vagrant@vagrant.vm>
2019-04-16 20:22:14 +0000

    testing.cc

--- service_layer_backend_testing.cc
+++ service_layer_backend_testing.cc
@@ -1,6 +1,10 @@
 #include "service_layer_backend_testing.h"
-
 #include <iostream>
+#include <optional>
+#include "utils/regex.h"
+#include "utils/stream_helper.h"
+using utils::GetTime;
+using utils::HashParser;
 
 ServiceLayerBackEndTesting::ServiceLayerBackEndTesting() {}
 
@@ -61,6 +65,8 @@ std::string ServiceLayerBackEndTesting::Chirp(const std::string& username,
   // place the chirp in the "newest" key so that monitoring users may access it
   key_value_backend_.DeleteKey("newest");
   key_value_backend_.Put("newest", serialized_chirp);
+  // Parse the hashtag info from chirp ans send them to database
+  ChirpStream(ch);
   return "success";
 }
 
@@ -138,4 +144,103 @@ void ServiceLayerBackEndTesting::Monitor(
       }
     }
   }
+}
+// append current time to chirp and put chirps into their tag categories
+void ServiceLayerBackEndTesting::ChirpStream(chirp::Chirp& chirp) {
+  // Add timestamp to current chirp
+  auto time = GetTime();
+  chirp.mutable_timestamp()->set_seconds(time.seconds());
+  chirp.mutable_timestamp()->set_useconds(time.useconds());
+  auto text = chirp.text();
+  auto all_tags = HashParser(text);
+  std::string serial_chirp;
+  chirp.SerializeToString(&serial_chirp);
+  for (auto tag : all_tags) {
+    auto vec = key_value_backend_.Get(tag);
+    // ensure that tag always map to the most updated chirp
+    if (vec.size() == 0) {
+      key_value_backend_.Put(tag, serial_chirp);
+    } else {
+      key_value_backend_.DeleteKey(tag);
+      key_value_backend_.Put(tag, serial_chirp);
+    }
+  }
+}
+
+// Get hashtag's chirp through tag and return std::nullopt if
+// such tag have no chirp
+std::optional<chirp::Chirp> ServiceLayerBackEndTesting::GetTagInfo(
+    const std::string& tag) {
+  auto vec = key_value_backend_.Get(tag);
+  if (vec.size() != 0) {
+    chirp::Chirp chirp;
+    chirp.ParseFromString(vec[0]);
+    return chirp;
+  }
+  return std::nullopt;
+}
+
+// Stream service, keep streaming request hash tags
+// and send results to reply
+// Can synchronize with StreamBuffer function to buffer
+// reply information to a vector
+void ServiceLayerBackEndTesting::Stream(const chirp::StreamRequest* request,
+                                        chirp::StreamReply* reply) {
+  auto tag = request->hashtag();
+  auto time_interval = stream_refresh_timeval_;
+  int64_t curr_loop = 0;
+  auto time = GetTime();
+  while (curr_loop != stream_refresh_times_) {
+    std::this_thread::sleep_for(time_interval);
+    auto chirp = GetTagInfo(tag);
+    if (chirp && (*chirp).timestamp().useconds() > time.useconds()) {
+      std::unique_lock<std::mutex> monitor_lk(stream_mutex_);
+      if (stream_buff_mode_) {
+        stream_buf_signal_.wait(monitor_lk, [this] { return !stream_flag_; });
+      }
+      StreamSet(reply, *chirp);
+      stream_flag_ = true;
+      monitor_lk.unlock();
+      stream_buf_signal_.notify_one();
+      time.set_useconds((*chirp).timestamp().useconds());
+    }
+    if (stream_refresh_times_ != -1) {
+      curr_loop++;
+    }
+  }
+  std::lock_guard<std::mutex> lk(stream_mutex_);
+  stream_exit_flag_ = true;
+  stream_refresh_times_ = -1;
+  stream_buf_signal_.notify_one();
+  return;
+}
+//  Stream Buffer Service
+std::thread ServiceLayerBackEndTesting::StreamBuffer(
+    const chirp::StreamReply* reply, vector<chirp::Chirp>& buffer) {
+  auto lock_cond = [this] { return stream_exit_flag_ || stream_flag_; };
+  std::thread thr([this, reply, lock_cond, &buffer] {
+    while (true) {
+      std::unique_lock<std::mutex> monitor_lk(stream_mutex_);
+      stream_buf_signal_.wait(monitor_lk, lock_cond);
+      if (stream_exit_flag_) return;
+      chirp::Chirp curr = reply->chirp();
+      buffer.push_back(curr);
+      stream_flag_ = false;
+      monitor_lk.unlock();
+      stream_buf_signal_.notify_all();
+    }
+  });
+  return thr;
+}
+
+void ServiceLayerBackEndTesting::StreamSet(chirp::StreamReply* reply,
+                                           const chirp::Chirp& reply_chirp) {
+  reply->mutable_chirp()->set_id(reply_chirp.id());
+  reply->mutable_chirp()->set_username(reply_chirp.username());
+  reply->mutable_chirp()->set_text(reply_chirp.text());
+  reply->mutable_chirp()->set_parent_id(reply_chirp.parent_id());
+  reply->mutable_chirp()->mutable_timestamp()->set_seconds(
+      reply_chirp.timestamp().seconds());
+  reply->mutable_chirp()->mutable_timestamp()->set_useconds(
+      reply_chirp.timestamp().useconds());
 }
